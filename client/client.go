024c277b27bbb9dbd2913e5c43d444b3fb2881e6
Erik Lima <erik.tain@gmail.com>
2023-06-11 13:47:37 -0300

    Criar um loop para simular vários clientes

--- client/client.go
+++ client/client.go
@@ -13,6 +13,7 @@ import (
 	"os"
 	"path"
 	"runtime"
+	"sync"
 	"time"
 
 	"golang.org/x/net/http2"
@@ -46,128 +47,166 @@ func setupCertPath() {
 }
 
 func main() {
-	// buf := createBuf(1000 * 1000 * 1000)
-	buf := createBuf(0)
+	loopCount := 20
+	var wg sync.WaitGroup
 
-	var req *http.Request
-	var err error
-	// host := "https://192.168.1.8:3443"
-	host := "https://localhost:3443"
+	wg.Add(loopCount)
 
-	if len(*buf) == 0 {
-		req, err = http.NewRequest("GET", host, nil)
-		if err != nil {
-			log.Fatalf("unable to create request: %v", err)
-		}
-	} else {
-		req, err = http.NewRequest("POST", host, bytes.NewReader(*buf))
-		if err != nil {
-			log.Fatalf("unable to create request: %v", err)
-		}
-	}
+	for loopCount > 0 {
+		loopCount -= 1
 
-	fmt.Printf("METHOD :::: %s\n", req.Method)
+		go func() {
+			// buf := createBuf(1000 * 1000 * 1000)
+			buf := createBuf(0)
 
-	var t0, t1, t2, t3, t4, t5, t6, tGetConn time.Time
+			var req *http.Request
+			var err error
+			// host := "https://localhost:3443"
+			// host := "https://192.168.1.8:3443"
+			host := "https://193.167.100.100:3443"
 
-	// TODO:: Verificar se os ts foram definidos antes de calcular o tempo
-
-	trace := &httptrace.ClientTrace{
-		ConnectStart: func(_, _ string) {
-			if t1.IsZero() {
-				// connecting to IP
-				t1 = time.Now()
+			if len(*buf) == 0 {
+				req, err = http.NewRequest("GET", host, nil)
+				if err != nil {
+					log.Fatalf("unable to create request: %v", err)
+				}
+			} else {
+				req, err = http.NewRequest("POST", host, bytes.NewReader(*buf))
+				if err != nil {
+					log.Fatalf("unable to create request: %v", err)
+				}
 			}
-			fmt.Println("Connecting to IP...")
-		},
-		ConnectDone: func(net, addr string, err error) {
+
+			// fmt.Printf("METHOD :::: %s\n", req.Method)
+
+			tInit := time.Now()
+
+			var t0, t1, t2, t3, t4, t5, t6, tGetConn time.Time
+
+			// TODO:: Verificar se os ts foram definidos antes de calcular o tempo
+
+			trace := &httptrace.ClientTrace{
+				ConnectStart: func(_, _ string) {
+					if t1.IsZero() {
+						// connecting to IP
+						t1 = time.Now()
+					}
+					fmt.Println("EVENT: ConnectStart", t1.Sub(tInit))
+				},
+				ConnectDone: func(net, addr string, err error) {
+					if err != nil {
+						log.Fatalf("unable to connect to host %v: %v", addr, err)
+					}
+					t2 = time.Now()
+
+					fmt.Println("EVENT: ConnectDone", t2.Sub(tInit))
+				},
+				DNSStart: func(_ httptrace.DNSStartInfo) { t0 = time.Now() },
+				DNSDone:  func(_ httptrace.DNSDoneInfo) { t1 = time.Now() },
+				GetConn: func(_ string) {
+					tGetConn = time.Now()
+					fmt.Println("EVENT: GetConn", tGetConn.Sub(tInit))
+				},
+				GotConn: func(_ httptrace.GotConnInfo) {
+					t3 = time.Now()
+					fmt.Println("EVENT: GotConn", t3.Sub(tInit))
+				},
+				GotFirstResponseByte: func() {
+					t4 = time.Now()
+					fmt.Println("EVENT: GotFirstResponseByte", t4.Sub(tInit))
+				},
+				TLSHandshakeStart: func() {
+					t5 = time.Now()
+					fmt.Println("EVENT: TLSHandshakeStart", t5.Sub(tInit))
+				},
+				TLSHandshakeDone: func(_ tls.ConnectionState, err error) {
+					t6 = time.Now()
+					if err != nil {
+						log.Fatalf("failed to perform TLS handshake: %v", err)
+					}
+					fmt.Println("EVENT: TLSHandshakeDone", t6.Sub(tInit))
+				},
+			}
+
+			req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
+
+			client := &http.Client{
+				Transport: transportHttp2(),
+			}
+
+			res, err := client.Do(req)
 			if err != nil {
-				log.Fatalf("unable to connect to host %v: %v", addr, err)
+				log.Fatalf("failed to read response: %v", err)
 			}
-			t2 = time.Now()
 
-			fmt.Printf("\nConnected to %s\n", addr)
-		},
-		DNSStart:             func(_ httptrace.DNSStartInfo) { t0 = time.Now() },
-		DNSDone:              func(_ httptrace.DNSDoneInfo) { t1 = time.Now() },
-		GetConn:              func(_ string) { tGetConn = time.Now() },
-		GotConn:              func(_ httptrace.GotConnInfo) { t3 = time.Now() },
-		GotFirstResponseByte: func() { t4 = time.Now() },
-		TLSHandshakeStart:    func() { t5 = time.Now() },
-		TLSHandshakeDone: func(_ tls.ConnectionState, err error) {
-			t6 = time.Now()
-			if err != nil {
-				log.Fatalf("failed to perform TLS handshake: %v", err)
+			t7 := time.Now() // after read body
+			fmt.Println("EVENT: ResponseReady", t7.Sub(tInit))
+
+			// res, err := client.Get("https://localhost:3443")
+			// if err != nil {
+			// 	log.Fatal(err)
+			// }
+
+			bodySize := res.Header.Get("X-Body-Size")
+
+			// body := getBody(res)
+			// responseLength := res.Body.
+			// fmt.Printf("Body size: %v\n", body)
+			// fmt.Printf("Body size Recalc: %d\n", len(body))
+
+			res.Body.Close()
+
+			// t7 := time.Now() // after read body
+
+			dnsLookup := t1.Sub(t0) // dns lookup
+			// tcpConnection := t2.Sub(t1)    // tcp connection
+			tcpConnection := t3.Sub(tGetConn) // tcp connection
+			tlsHandshake := t6.Sub(t5)        // tls handshake
+			serverProcessing := t4.Sub(t3)    // server processing
+			contentTransfer := t7.Sub(t3)     // content transfer
+			connect := t2.Sub(t1)             // connect
+			preTransfer := t3.Sub(t0)         // pretransfer
+			startTransfer := t4.Sub(t0)       // starttransfer
+			// total := t7.Sub(t0)            // total
+			total := t7.Sub(tGetConn) // total
+			// total := t7.Sub(t1) // total
+
+			metrics := &Metrics{
+				dnsLookup:        dnsLookup,
+				tcpConnection:    tcpConnection,
+				tlsHandshake:     tlsHandshake,
+				serverProcessing: serverProcessing,
+				contentTransfer:  contentTransfer,
+				connect:          connect,
+				preTransfer:      preTransfer,
+				startTransfer:    startTransfer,
+				total:            total,
 			}
-		},
+
+			saveMetrics(metrics)
+
+			fmt.Printf("\nProtocol: %s %s\n", res.Proto, req.Method)
+			fmt.Printf("Code: %d\n", res.StatusCode)
+			// fmt.Printf("Body: %s\n", body)
+			// fmt.Printf("Body Length: %d\n\n", responseLength)
+			fmt.Printf("Body Size Header: %s\n", bodySize)
+
+			// fmt.Printf("\nDNS Lookup: %s\n", dnsLookup)
+			fmt.Printf("\nConnection time: %s\n", tcpConnection)
+			// fmt.Printf("TLS handshake: %s\n", tlsHandshake)
+			// fmt.Printf("Server processing: %s\n", serverProcessing)
+			fmt.Printf("Content transfer: %s\n", contentTransfer)
+			// fmt.Printf("Connection: %s\n", connect)
+			// fmt.Printf("Pretransfer: %s\n", preTransfer)
+			// fmt.Printf("Starttransfer: %s\n", startTransfer)
+			fmt.Printf("Total: %s\n", total)
+			fmt.Print("-----------------------------------------\n")
+
+			wg.Done()
+		}()
 	}
 
-	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
-
-	client := &http.Client{
-		Transport: transportHttp2(),
-	}
-
-	res, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("failed to read response: %v", err)
-	}
-
-	t7 := time.Now() // after read body
-
-	// res, err := client.Get("https://localhost:3443")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// body := getBody(res)
-	// responseLength := res.Body.
-
-	res.Body.Close()
-
-	// t7 := time.Now() // after read body
-
-	dnsLookup := t1.Sub(t0) // dns lookup
-	// tcpConnection := t2.Sub(t1)    // tcp connection
-	tcpConnection := t3.Sub(tGetConn) // tcp connection
-	tlsHandshake := t6.Sub(t5)        // tls handshake
-	serverProcessing := t4.Sub(t3)    // server processing
-	contentTransfer := t7.Sub(t4)     // content transfer
-	connect := t2.Sub(t1)             // connect
-	preTransfer := t3.Sub(t0)         // pretransfer
-	startTransfer := t4.Sub(t0)       // starttransfer
-	// total := t7.Sub(t0)            // total
-	total := t7.Sub(tGetConn) // total
-	// total := t7.Sub(t1) // total
-
-	metrics := &Metrics{
-		dnsLookup:        dnsLookup,
-		tcpConnection:    tcpConnection,
-		tlsHandshake:     tlsHandshake,
-		serverProcessing: serverProcessing,
-		contentTransfer:  contentTransfer,
-		connect:          connect,
-		preTransfer:      preTransfer,
-		startTransfer:    startTransfer,
-		total:            total,
-	}
-
-	saveMetrics(metrics)
-
-	fmt.Printf("Protocol: %s\n", res.Proto)
-	fmt.Printf("Code: %d\n", res.StatusCode)
-	// fmt.Printf("Body: %s\n", body)
-	// fmt.Printf("Body Length: %d\n\n", responseLength)
-
-	// fmt.Printf("\nDNS Lookup: %s\n", dnsLookup)
-	fmt.Printf("Connection time: %s\n", tcpConnection)
-	// fmt.Printf("TLS handshake: %s\n", tlsHandshake)
-	fmt.Printf("Server processing: %s\n", serverProcessing)
-	fmt.Printf("Content transfer: %s\n", contentTransfer)
-	// fmt.Printf("Connection: %s\n", connect)
-	// fmt.Printf("Pretransfer: %s\n", preTransfer)
-	// fmt.Printf("Starttransfer: %s\n", startTransfer)
-	fmt.Printf("Total: %s\n", total)
+	wg.Wait()
 }
 
 func transportHttp2() *http2.Transport {
@@ -177,7 +216,8 @@ func transportHttp2() *http2.Transport {
 }
 
 func tlsConfig() *tls.Config {
-	crt, err := os.ReadFile(path.Join(certPath, "cert.pem"))
+	// crt, err := os.ReadFile(path.Join(certPath, "cert.pem"))
+	crt, err := os.ReadFile("./cert.pem")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -205,7 +245,8 @@ func tlsConfig() *tls.Config {
 // }
 
 func saveMetrics(metrics *Metrics) {
-	f, err := os.OpenFile(path.Join(certPath, "metrics.csv"), os.O_WRONLY|os.O_APPEND, os.ModeAppend)
+	// f, err := os.OpenFile(path.Join(certPath, "metrics.csv"), os.O_WRONLY|os.O_APPEND, os.ModeAppend)
+	f, err := os.OpenFile("./metrics.csv", os.O_WRONLY|os.O_APPEND, os.ModeAppend)
 	if err != nil {
 		// fmt.Println(err)
 		// return
